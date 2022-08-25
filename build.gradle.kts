85677fe9bc5998beb8f69f72ff0ea83e8e9ac12b
anmolsinghsahi <anmol.sahi@mutualmobile.com>
2022-08-25 16:03:28 +0530

    initial commit: setup kmm project

---
+++ build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    kotlin("multiplatform") version "1.7.10"
+    id("com.android.application")
+    id("kotlin-android-extensions")
+}
+
+group = "me.anmolsinghsahi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(BOTH) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    android()
+    iosArm64 {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    iosX64 {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.5.0")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+        val iosArm64Main by getting
+        val iosArm64Test by getting
+        val iosX64Main by getting
+        val iosX64Test by getting
+    }
+}
+
+android {
+    compileSdkVersion(31)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        applicationId = "me.anmolsinghsahi.library"
+        minSdkVersion(24)
+        targetSdkVersion(31)
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
